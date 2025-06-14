453c9fa65b94afde6f65d5df0861aa3cd7523b53
Alisenaqubadi <131773898+Alisenaqubadi@users.noreply.github.com>
2025-06-14 17:14:58 +0500

    input is ready

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const myLibrary = [];
 const openButton = document.querySelector("[data-open-modal]");
 const closeButton = document.querySelector("[data-close-modal]");
 const modal = document.querySelector("[data-modal]");
+const title = document.getElementById("title");
+const author = document.getElementById("author");
+const pages = document.getElementById("pages");
+const read = document.getElementById("read");
+const form = document.querySelector("form");
+const savebutton = document.getElementById('savebutton');
 
 function Book(title,author,pages,read)
 {
@@ -25,4 +31,18 @@ openButton.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
     modal.close()
+})
+
+form.addEventListener("submit" , (e) => {
+    e.preventDefault();
+
+    const newTitle = title.value;
+    const newAuthor = author.value;
+    const newPages = pages.value;
+    const isRead = read.checked;
+
+    pushtolib(newTitle,newAuthor,newPages,isRead);
+
+    form.reset();
+    modal.close();
 })
